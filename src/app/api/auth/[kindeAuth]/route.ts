4c390cd2252031bdc584ce3542472b39d4ed01ec
Daniel Fraga <dan.fraga33@gmail.com>
2024-02-06 16:35:03 +0000

    feat: dashboard + styles

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,6 +1,7 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
 export async function GET(request: NextRequest, { params }: any) {
-	const endPoint = params.kindeAuth;
-	return handleAuth(request, endPoint);
+	const endpoint = params.kindeAuth;
+	return handleAuth(request, endpoint);
 }
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import { trpc } from '@/app/_trpc/client';
 import { PropsWithChildren, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
-import { trpc } from '@/app/_trcp/client';
 
 const Providers = ({ children }: PropsWithChildren) => {
 	const [queryClient] = useState(() => new QueryClient());
