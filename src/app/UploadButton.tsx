3e8b1e01d76b1c55224fcfb792413abb16ece433
Daniel Fraga <dan.fraga33@gmail.com>
2024-02-08 22:50:34 +0000

    fix: removed state from upload button component

--- src/app/UploadButton.tsx
+++ src/app/UploadButton.tsx
@@ -5,21 +5,13 @@ import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { useState } from 'react';
 
 const UploadButton = () => {
-	const [isOpen, setIsOpen] = useState<boolean>(false);
 	return (
-		<Dialog
-			open={isOpen}
-			onOpenChange={(v: any) => {
-				if (!v) {
-					setIsOpen(v);
-				}
-			}}
-		>
+		<Dialog>
 			<DialogTrigger asChild>
 				<Button>Upload PDF</Button>
 			</DialogTrigger>
 
-			<DialogContent>Hello</DialogContent>
+			<DialogContent>asdasd</DialogContent>
 		</Dialog>
 	);
 };
