cf5d10dc0d7c794c61189ac87c3a5d1ed5407400
Daniel Fraga <dan.fraga33@gmail.com>
2024-02-09 22:52:20 +0000

    fix: rearrange imports

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
+import Dashboard from '@/components/Dashboard';
+import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
-import { db } from '@/db';
-import Dashboard from '../../components/Dashboard';
 
 const Page = async () => {
 	const { getUser } = getKindeServerSession();
-	const user = await getUser();
+	const user = getUser();
 
 	if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
 
