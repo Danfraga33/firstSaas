2369d594cbea9408bee425374f9bf0442da80f73
Daniel Fraga <dan.fraga33@gmail.com>
2024-02-13 13:44:50 +0000

    style: adding loading animation for pdf

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -72,11 +72,26 @@ const UploadDropZone = () => {
 							{isUploading ? (
 								<div className="w-full mt-4 max-w-xs mx-auto">
 									<Progress
+										indicatorColor={
+											uploadProgress === 100 ? 'bg-green-500' : ''
+										}
 										value={uploadProgress}
 										className="h-1 w-full bg-zinc-200"
 									/>
+									{uploadProgress === 100 ? (
+										<div className="flex gap-2 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+											<Loader className="h-3 w-3 animate-spin " />
+											Redirecting...
+										</div>
+									) : null}
 								</div>
 							) : null}
+							<input
+								{...getInputProps}
+								type="file"
+								id="dropzone-file"
+								className="hidden"
+							/>
 						</label>
 					</div>
 				</div>
