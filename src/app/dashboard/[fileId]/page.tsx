b7a5211e3dec471337c700be3c0deb19a1dc8f95
Daniel Fraga <dan.fraga33@gmail.com>
2024-02-15 10:48:29 +0000

    fix: String => string

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -6,7 +6,7 @@ import { notFound, redirect } from 'next/navigation';
 
 interface PageProps {
 	params: {
-		fileId: String;
+		fileId: string;
 	};
 }
 
@@ -32,12 +32,12 @@ const Page = async ({ params }: PageProps) => {
 			<div className="mx-auto w-full max-w-8xl grow lg:flex xl: xl:px-2">
 				{/* left side */}
 				<div className="flex-1 xl:flex">
-					<div className="px-4 py-7 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+					<div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
 						<PdfRenderer url={file.url} />
 					</div>
 				</div>
 				{/* Right Side */}
-				<div className="shrink-0 flex-[0.75] border-t b-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+				<div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
 					<ChatWrapper />
 				</div>
 			</div>
