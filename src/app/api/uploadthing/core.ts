b290dfd1421c54d59844121cb81826758979e082
Daniel Fraga <dan.fraga33@gmail.com>
2024-02-18 18:31:36 +0000

    fix: adjusting URL to ensure successful pdf load

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,10 @@
 import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
-
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { pinecone } from '@/lib/pinecone';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { PineconeStore } from '@langchain/pinecone';
 const f = createUploadthing();
 
 const auth = (req: Request) => ({ id: 'fakeId' });
@@ -10,7 +13,6 @@ export const ourFileRouter = {
 	pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
 		.middleware(async ({ req }) => {
 			const { getUser } = getKindeServerSession();
-
 			const user = getUser();
 
 			if (!user.id || !user) throw new Error('Unauthorized');
@@ -23,10 +25,54 @@ export const ourFileRouter = {
 					key: file.key,
 					name: file.name,
 					userId: metadata.userId,
-					url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-					uploadedStatus: 'PROCESSING',
+					url: `https://utfs.io/f/${file.key}`,
+					uploadStatus: 'PROCESSING',
 				},
 			});
+
+			try {
+				const response = await fetch(`https://utfs.io/f/${file.key}`);
+				// PDF
+				const blob = await response.blob();
+				// WE load the PDF into the Langchain
+				const loader = new PDFLoader(blob);
+
+				const pageLevelDocs = await loader.load();
+
+				const pagesAnt = pageLevelDocs.length;
+
+				// Vectorize and index entire doc
+				const pineconeIndex = pinecone.index('firstsaas');
+
+				const embeddings = new OpenAIEmbeddings({
+					openAIApiKey: process.env.OPENAI_API_KEY,
+				});
+
+				await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+					//@ts-ignore
+					pineconeIndex,
+					namespace: createdFile.id,
+				});
+
+				await db.file.update({
+					data: {
+						uploadStatus: 'SUCCESS',
+					},
+					where: {
+						id: createdFile.id,
+					},
+				});
+			} catch (err) {
+				await db.file.update({
+					data: {
+						uploadStatus: 'FAILED',
+					},
+					where: {
+						id: createdFile.id,
+					},
+				});
+				console.error(err);
+			}
 		}),
 } satisfies FileRouter;
 
