d9732b0eff357edf0ad2a761d2bccc364e9bf370
Daniel Fraga <dan.fraga33@gmail.com>
2024-03-07 16:24:29 +0000

    chore: changing app title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 			<MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
 				<div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
 					<p className="text-sm font-semibold text-gray-700">
-						Quill is now public
+						The Investment Hub is now public
 					</p>
 				</div>
 				<h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
@@ -131,8 +131,8 @@ export default function Home() {
 								Start asking questions
 							</span>
 							<span className="mt-2 text-zinc-700">
-								It&apos;s that simple. Try out quill today. It really only takes
-								a minute.
+								It&apos;s that simple. Try out IH today. It really only takes a
+								minute.
 							</span>
 						</div>
 					</li>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 			<MaxWidthWrapper>
 				<div className="flex h-14 items-center justify-between border-b border-zinc-200 ">
 					<Link href="/" className="flex z-40 font-semibold">
-						<span>quill.</span>
+						<span>asdasd</span>
 					</Link>
 					{/* To do: add mobile navbar */}
 					<div className="hidden items-center space-x-4 sm:flex">
